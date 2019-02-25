826dc1052f9d9e8f183bac4971906f528a5b1638
Pauline De Smet <paulinedesmet@me.com>
2019-02-25 14:09:25 +0100

    work

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require ('express')
 const app = express ()
 
-app.set('view engine', 'ejs')
+// app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-    res.render('index')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 server = app.listen(3000)
