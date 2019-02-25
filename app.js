43b921a528d427f5022e0e717bfda6771222d4cf
Pauline De Smet <paulinedesmet@me.com>
2019-02-25 16:19:45 +0100

    werk

--- app.js
+++ app.js
@@ -9,40 +9,41 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+
 io.on('connection', socket => {
     console.log('a user connected');
+    
+  	//default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
 
-    socket.on('chat message',  msg => {
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
-    });
+    // socket.on('chat message',  msg => {
+    //     io.emit('chat message', msg);
+    //     console.log('message: ' + msg);
+    // });
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
 });
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 
 
-// io.on('connection', function(socket){
-//     socket.broadcast.emit('hi')
-// })
 
 
-    // //default username
-    // socket.username= "anonymous"
 
-    // //change username
-    // socket.on('change_username', (data)=> {
-    //     socket.username=data.username
-    // })
+  
 
-    // //message
-    // socket.on('new_message',(data) =>{
-    //     io.sockets.emit('new_message', {message : data.message, username : socket.username})
-    // })
-// })
+  
 
 
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,41 +1,43 @@
-$(function(){
+$(function () {
     //connection
     let socket = io.connect('http://localhost:3000')
 
-    $('form').submit(function(e){
-        e.preventDefault();
-        
-        socket.emit('chat message', $('#m').val())
-        $('#m').val('')
-        return false;
-    })
 
-    
-    socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg))
-    })
     // //button
-    // let message = $('#message')
-    // let username = $('#username')
-    // let send_message = $('#send_message')
-    // let send_username = $('#send_username')
-    // let chatroom = $('#chatroom')
+    let message = $('#message')
+    let username = $('#username')
+    let send_message = $('#send_message')
+    let send_username = $('#send_username')
+    let chatroom = $('#chatroom')
 
-    // //username
-    // send_username.click(function(){
-    //     console.log(username.val())
-    //     socket.emit('change_username',{username: username.val()})
+
+    // $('form').submit(function(e){
+    //     e.preventDefault();
+
+    //     socket.emit('chat message', $('#m').val())
+    //     $('#m').val('')
+    //     return false;
     // })
 
-    // // new message
-    // send_message.click(function(){
-    //     socket.emit('new_message', {message: message.val()})
-    // })
+   
 
-    // //luistert naart nieuw berichtje
-    // socket.on('new_message', (data) => {
-    //     console.log(data)
-    //     chatroom.append("<p> class='message'</p>" + data.username + ': ' + data.send_message + '</p>')
-    // })
+    send_message.click(function () {
+        socket.emit('new_message', {
+            message: message.val()
+        })
+    })
 
+    //Listen on new_message
+    socket.on("new_message", (data) => {
+        message.val('');
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+     //username
+     send_username.click(function () {
+        console.log(username.val())
+        socket.emit('change_username', {
+            username: username.val()
+        })
+    })
 });
