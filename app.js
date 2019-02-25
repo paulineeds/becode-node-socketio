fd16d8053117a83e6e880c75418e8b4e8b6b7a6c
Pauline De Smet <paulinedesmet@me.com>
2019-02-25 14:18:51 +0100

    idk

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-const express = require ('express')
-const app = express ()
+const express = require('express');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
-// app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/index.html')
-})
-
-server = app.listen(3000)
-
-
-//socket.io
-const io = require ('socket.io').listen(server);
-
-io.on('connection',(socket) => {
-    console.log('New user connected')
-    socket.on('disconnect', function(){
+io.on('connection', socket => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
         console.log('user disconnected')
     })
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg)
-        console.log('message: ' + msg)
-    })
+
+    socket.on('chat message',  msg => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
 });
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
 
 
 // io.on('connection', function(socket){
